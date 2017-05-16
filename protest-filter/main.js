2d2d76bbc56a7c62f1d1f10ea14230c00bf35c79
Ryan Kelley <ryankelley@Ryans-MacBook-Pro.local>
2017-05-16 16:53:56 -0400

    adds more content

--- protest-filter/main.js
+++ protest-filter/main.js
@@ -3,12 +3,13 @@ $(document).ready(function () {
 		e.preventDefault();
 		var button = $(this);
 		var filter = button.data("filter");
-
+		//this stores the data attribute
 		$(".item.active").removeClass("active").removeClass("flipped");
-
+		//this ensures that when you've already clicked one filter, when you click another it will show the front image
 		if (filter !== "reset") {
 			$("." + filter).addClass("active");
 		}
+		//adds the active class to make each button (except reset) show the image when clicked
 	});
 
 	$(".item").on("click", function (e) {
